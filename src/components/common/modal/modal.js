cfcdf9cfa1a29ecf445759e8097f8698777d35eb
Raxit Jain <raxit.jain@teck.com>
2021-07-13 08:16:35 +0530

    Updating Modal component to use styled components

--- src/components/common/modal/modal.js
+++ src/components/common/modal/modal.js
@@ -1,16 +1,34 @@
 import React, { Fragment } from 'react';
 import Backdrop from '../backdrop/backdrop';
-import './modal.css';
+import styled from 'styled-components';
+
+const ModalContainer = styled.div`
+    position: fixed;
+    z-index: 500;
+    background-color: white;
+    width: 70%;
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 1px black;
+    padding: 16px;
+    left: 15%;
+    top: 30%;
+    box-sizing: border-box;
+    transition: all 0.3s ease-out;
+    transform: ${(props) => props.show ? `translateY(0)` : `translateY(100vh)`};
+    opacity: ${(props) => props.show ? `1` : `0`};
+    @media (min-width: 600px) {
+        width: 500px;
+        left: calc(50% - 250px);
+    }
+`;
 
 const Modal = (props) => {
-
-    const classes = ['Modal', props.show ? 'show-modal' : 'hide-modal'];
     return (
         <Fragment>
             <Backdrop show={props.show} clicked={props.modalClosed} />
-            <div className={classes.join(' ')}>
+            <ModalContainer show={props.show}>
                 {props.children}
-            </div>
+            </ModalContainer>
         </Fragment>
     );
 };
